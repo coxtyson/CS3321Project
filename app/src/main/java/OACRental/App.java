d4e5780e9d3d4609fe294bb8ccf93df2619d08ad
DalenPlanestrider <tysoncoxwins@gmail.com>
2023-01-31 16:58:41 -0700

    Javafx hello world test

--- app/src/main/java/OACRental/App.java
+++ app/src/main/java/OACRental/App.java
@@ -3,12 +3,39 @@
  */
 package OACRental;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World");
+        Button btn = new Button("Hello World");
+
+        btn.setOnAction(
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        System.out.println("Hello!");
+                    }
+                }
+        );
+
+        StackPane pane = new StackPane();
+        pane.getChildren().add(btn);
+
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
